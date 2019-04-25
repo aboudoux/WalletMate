55c51c04e320ac0ca22aedb3588a648b4da5bf84
Aurélien BOUDOUX <aurelien@boudoux.fr>
2019-04-25 11:41:18 +0200

    cryptage des mot de passe dans le fichier de configuration

--- Backend/WalletMate.Infrastructure.Tests/XmlConfigurationProviderShould.cs
+++ Backend/WalletMate.Infrastructure.Tests/XmlConfigurationProviderShould.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using FluentAssertions;
+using WalletMate.Infrastructure.Dto;
 using WalletMate.Infrastructure.Services;
 using WalletMate.Infrastructure.Tests.Tools;
 using Xunit;
@@ -15,15 +16,43 @@ namespace WalletMate.Infrastructure.Tests
         {
             await Make.TestFile("TestUser.config").AndExecute(environment =>
             {
-                var file = "TestUser.config";
-                Action action = () => new XmlConfigurationProvider(file);
+                Action action = () => new XmlConfigurationProvider(environment.FilePath);
 
                 action.Should().Throw<FileNotFoundException>();
-                File.Exists(file).Should().BeTrue();
-                new FileInfo(file).Length.Should().BePositive();
+                File.Exists(environment.FilePath).Should().BeTrue();
+                new FileInfo(environment.FilePath).Length.Should().BePositive();
 
                 return Task.CompletedTask;
             });
+        }
+
+        [Theory]
+        [InlineData("1234", "secret:3NcbCywQ+zhc+c43b/s3GkEx7kAEVMQfbqAvW3oUcfE3m3w3AGq+CLRuL8Hie2Oy")]
+        [InlineData("password", "secret:UvIikN+fDwhL4DVCZVeJ+EtH986jtXuzN3x5E76P5EHlii+X3zY3oArvnF1dBdrM")]
+        public async Task Crypt_password_if_stored_in_plain_text(string allPassword, string expected)
+        {
+            await Make.TestFile("password.config").AndExecute(environment =>
+            {
+                CreateConfigFile(environment.FilePath, firstUserPassword: allPassword, secondUserPassword:allPassword, operatorPassword:allPassword);
+
+                var _ = new XmlConfigurationProvider(environment.FilePath);
+
+                var config = Xml.DeserializeFrom<WalletMateConfiguration>(environment.FilePath);
+                config.FirstPair.Password.Should().Be(expected);
+                config.SecondPair.Password.Should().Be(expected);
+                config.Operator.Password.Should().Be(expected);
+
+                return Task.CompletedTask;
+            });
+        }
+
+        private static void CreateConfigFile(string fileName, string firstUsername = "FirstUserName", string firstUserPassword = "password", string secondUsername = "SecondUsername", string secondUserPassword = "password", string operatorUsername = "Operator", string operatorPassword = "password")
+        {
+            var config = new WalletMateConfiguration();
+            config.FirstPair = new User(firstUsername, firstUserPassword);
+            config.SecondPair = new User(secondUsername, secondUserPassword);
+            config.Operator = new User(operatorUsername, operatorPassword);
+            Xml.SaveTo(fileName, config);
         }
     }
 }
--- Backend/WalletMate.Infrastructure/Services/XmlConfigurationProvider.cs
+++ Backend/WalletMate.Infrastructure/Services/XmlConfigurationProvider.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
 using WalletMate.Domain.Common;
 using WalletMate.Infrastructure.Dto;
 
 namespace WalletMate.Infrastructure.Services
 {
     public class XmlConfigurationProvider : IConfigurationProvider
-    {
+    {       
         public XmlConfigurationProvider(string configurationFile = null)
         {
             if (configurationFile.IsEmpty())
@@ -19,21 +21,42 @@ namespace WalletMate.Infrastructure.Services
 
             if (!File.Exists(configurationFile))
             {
+                CreateDefaultConfiguration();
+                throw new FileNotFoundException("La configuration des utilisateurs n'a pas été trouvée. Un fichier a été généré pour que vous puissiez l'éditer");
+            }
+
+            var configuration = ConfigurationDeserializer.Deserialize(configurationFile);
+            
+
+            void CreateDefaultConfiguration()
+            {
                 var config = new WalletMateConfiguration();
                 config.FirstPair = new User("FirstUserName", "password");
                 config.SecondPair = new User("SecondUserName", "password");
                 config.Operator = new User("OperatorUserName", "password");
-                
                 Xml.SaveTo(configurationFile, config);
-
-                throw new FileNotFoundException("La configuration des utilisateurs n'a pas été trouvée. Un fichier a été généré pour que vous puissiez l'éditer");
             }
-
         }
 
         public IReadOnlyCollection<User> GetUsers()
         {
             throw null;
         }
+
+       
+    }
+
+    public class ConfigurationDeserializer
+    {
+
+        public static WalletMateConfiguration Deserialize(string configurationFile)
+        {
+            var configuration = Xml.DeserializeFrom<WalletMateConfiguration>(configurationFile);
+            if(configuration.EncodePlainTextPasswords())
+                Xml.SaveTo(configurationFile, configuration);
+
+            return configuration;
+
+        }        
     }
 }
