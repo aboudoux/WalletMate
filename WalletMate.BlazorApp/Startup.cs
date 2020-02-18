eceb7bea1f3d59cd3db2432c5ebae4d0a280d5bc
Aurélien BOUDOUX <aurelien@boudoux.fr>
2020-02-18 17:26:29 +0100

    modification page avec erreur chelou

--- WalletMate.BlazorApp/Startup.cs
+++ WalletMate.BlazorApp/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
