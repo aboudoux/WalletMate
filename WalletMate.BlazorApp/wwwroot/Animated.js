0d342ef4edd3476e08f5bb8017fea1049e4e70e8
Aurélien BOUDOUX <aurelien@boudoux.fr>
2020-03-03 16:08:16 +0100

    animation end function

---
+++ WalletMate.BlazorApp/wwwroot/Animated.js
@@ -0,0 +1,4 @@
+﻿var AnimatedComponent = AnimatedComponent || {};
+AnimatedComponent.animationend = function (element, dotNet) {
+    element.addEventListener('animationend', function() { dotNet.invokeMethodAsync("OnAnimationEnd") });
+};
